be62f742aa874126ba47d51cf81116a19ded64c2
Stephen Hodgkiss <stephenhodgkiss1@gmail.com>
2023-01-30 20:40:01 +0700

    Create location.php

---
+++ location.php
@@ -0,0 +1,9 @@
+<?php 
+
+// Based on the IP address, this simple piece of text gets the 2 digit country code
+
+$ip_address = $_SERVER['REMOTE_ADDR'];
+$geo_data = json_decode(file_get_contents("http://ip-api.com/json/{$ip_address}"));
+$country_code = $geo_data->countryCode; 
+echo $country_code;
+?>
