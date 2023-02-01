7e24392c84ca225a6e97eb02200e3d062980044f
Stephen Hodgkiss <stephenhodgkiss1@gmail.com>
2023-02-01 12:57:43 +0700

    Based on the IP address get the country code

--- location.php
+++ location.php
@@ -1,6 +1,6 @@
 <?php 
 
-// Based on the IP address, this simple piece of text gets the 2 digit country code
+// Based on the IP address, this simple piece of code gets the 2 digit country code
 
 $ip_address = $_SERVER['REMOTE_ADDR'];
 $geo_data = json_decode(file_get_contents("http://ip-api.com/json/{$ip_address}"));
