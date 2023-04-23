6a5b86cad9094461a163351ad29b618a0c4a042d
Steve Hodgkiss <stephenhodgkiss1@gmail.com>
2023-04-23 14:17:25 +0700

    Add continent

--- location.php
+++ location.php
@@ -1,9 +1,10 @@
 <?php 
 
-// Based on the IP address, this simple piece of code gets the 2 digit country code
+// Based on the IP address, this simple piece of code gets the 2 digit country and continent codes
 
 $ip_address = $_SERVER['REMOTE_ADDR'];
-$geo_data = json_decode(file_get_contents("http://ip-api.com/json/{$ip_address}"));
-$country_code = $geo_data->countryCode; 
+$geo_data = json_decode(file_get_contents("http://ip-api.com/json/{$ip_address}?fields=status,message,continent,continentCode,country,countryCode"));
+$continent_ode = $geo_data->continentCode;
+$country_code = $geo_data->countryCode;
 echo $country_code;
 ?>
