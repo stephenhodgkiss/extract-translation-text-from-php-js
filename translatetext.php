b1ab54e0c49aaf8a81cd5262ef278bdbe966596a
Steve Hodgkiss <stephenhodgkiss1@gmail.com>
2023-09-08 22:10:14 +0700

    Moved code to new script set_language.php

--- translatetext.php
+++ translatetext.php
@@ -1,18 +1,5 @@
 <?php
 
-$_lang = [];
-
-$langfile = 'lang/en.inc.php'; // you can start with an empty file until you have ran the extract script
-include($langfile);
-
-$jsonLang = json_encode($_lang);
-
-// loop around the $lanffile and replace *## with <strong> and ##* with </strong>
-foreach ($_lang as $key => $value) {
-    $_lang[$key] = str_replace("*## ", "<strong>",  $value);
-    $_lang[$key] = str_replace(" ##*", "</strong>", $_lang[$key]);
-}
-
 function TranslateText($text) {
     global $_lang;
     if (isset($_lang[$text])) {
