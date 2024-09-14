053473b77e7a76e9732b667f95c7a3751e6eae87
Steve Hodgkiss <stephenhodgkiss1@gmail.com>
2024-09-14 17:00:33 +0700

    Update translatetext.js

--- translatetext.js
+++ translatetext.js
@@ -1,18 +1,17 @@
-const jsonLang = `<?php echo str_replace("'","\'",$jsonLang); ?>`
+const browserLanguage = "<?= $lang ?>";
 
+const jsonLang = '<?= str_replace("'", "\'", $jsonLang) ?>';
 var langArray = [];
-
-if (jsonLang == null) {
-} else {
+if (jsonLang == null) {} else {
     langArray = JSON.parse(jsonLang);
 }
 
 function TranslateText(textToTranslate) {
-    let translatedText = ''
-    if(langArray === null || langArray[textToTranslate] === null || langArray[textToTranslate] === undefined || langArray[textToTranslate] == '') {
+    if (langArray === null || langArray[textToTranslate] === null || langArray[textToTranslate] === undefined || langArray[textToTranslate] == '') {
         translatedText = textToTranslate;
     } else {
         translatedText = langArray[textToTranslate];
     }
+
     return translatedText;
 }
