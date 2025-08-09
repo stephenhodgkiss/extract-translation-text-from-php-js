7367072eb08565af30a38c7a90c0aef3fc496bd6
Steve Hodgkiss <stephenhodgkiss1@gmail.com>
2025-08-09 17:44:14 +0700

    Added additonal languages

--- get_language.php
+++ get_language.php
@@ -3,6 +3,8 @@
 function getLanguage($code)
 {
   switch ($code) {
+    case 'AF':
+      return 'Afrikaans';
     case 'AL':
       return 'Shqip';
     case 'AD':
@@ -17,6 +19,8 @@ function getLanguage($code)
       return 'Bosanski jezik';
     case 'BG':
       return 'Български';
+    case 'CS':
+      return 'Čeština';
     case 'GB':
       return 'English';
     case 'EN':
@@ -25,18 +29,26 @@ function getLanguage($code)
       return 'Hrvatski';
     case 'CY':
       return 'Ελληνικά';
+    case 'CA':
+      return 'Català';
     case 'CZ':
       return 'Čeština';
+    case 'DA':
+      return 'Dansk';
     case 'DK':
       return 'Dansk';
     case 'EE':
       return 'Eesti keel';
+    case 'ET':
+      return 'Eesti';
     case 'FI':
       return 'Suomi';
     case 'FR':
       return 'Français';
     case 'DE':
       return 'Deutsch';
+    case 'EL':
+      return 'Ελληνικά';
     case 'GR':
       return 'Ελληνικά';
     case 'HU':
@@ -47,8 +59,10 @@ function getLanguage($code)
       return 'Gaeilge';
     case 'IT':
       return 'Italiano';
+    case 'KO':
+      return '한국어';
     case 'LV':
-      return 'Latviešu valoda';
+      return 'Latviešu';
     case 'LI':
       return 'Deutsch';
     case 'LT':
@@ -69,10 +83,16 @@ function getLanguage($code)
       return 'Македонски јазик';
     case 'NO':
       return 'Norsk';
+    case 'NB':
+      return 'Norsk bokmål';
     case 'PL':
       return 'Polski';
     case 'PT':
       return 'Português';
+    case 'PT-BR':
+      return 'Português (Brasil)';
+    case 'PT-PT':
+      return 'Português (Portugal)';
     case 'RO':
       return 'Română';
     case 'RU':
@@ -89,9 +109,13 @@ function getLanguage($code)
       return 'Español';
     case 'SE':
       return 'Svenska';
+    case 'SV':
+      return 'Svenska';
     case 'CH':
       return 'Schwyzerdütsch';
     case 'UA':
+      return 'Українська';
+    case 'UK':
       return 'Українська';
     case 'VA':
       return 'Italiano';
@@ -99,6 +123,12 @@ function getLanguage($code)
       return 'Shqip/Srpski';
     case 'CN':
       return '中文';
+    case 'ZH':
+      return '中文';
+    case 'ZH-HANS':
+      return '简体中文';
+    case 'ZH-HANT':
+      return '繁體中文';
     case 'JA':
       return '日本語';
     case 'UR':
@@ -108,11 +138,25 @@ function getLanguage($code)
     case 'HI':
       return 'हिन्दी';
     case 'ID':
-      return 'Bahasa Indo';
+      return 'Bahasa Indonesia';
     case 'AR':
       return 'العربية';
     case 'SW':
       return 'Kiswahili';
+    case 'EN-GB':
+      return 'English (UK)';
+    case 'EN-US':
+      return 'English (US)';
+    case 'HE':
+      return 'עברית';
+    case 'TH':
+      return 'ไทย';
+    case 'VI':
+      return 'Tiếng Việt';
+    case 'SL':
+      return 'Slovenščina';
+    case 'TR':
+      return 'Türkçe';
     default:
       return $code;
   }
